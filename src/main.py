be78eae0b03d1dc92982f9efab3bfa247821d6bd
Ambashadur <womenjincha@gmail.com>
2023-11-03 15:53:26 +0300

    Add description to README.md

--- src/main.py
+++ src/main.py
@@ -15,8 +15,18 @@ def get_path_from_arguments(args) -> str:
 
 def main():
     path = get_path_from_arguments(sys.argv[1:])
-    # reader = TextDataReader()
-    reader = YamlDataReader()
+    file_extension = path.split('.')[-1]
+
+    reader = None
+
+    if file_extension == 'txt':
+        reader = TextDataReader()
+    elif file_extension == 'yaml':
+        reader = YamlDataReader()
+
+    if reader is None:
+        print('Unsopperted file extension!')
+
     students = reader.read(path)
     print("Students: ", students)
     rating = CalcRating(students).calc()
